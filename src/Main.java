755e45828e722221e042144d7cc71b52885c759e
CkBo3H9k <ajleksahgpbl4@mail.ru>
2023-02-11 00:31:48 +0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] array = new int[][] {
+                {65},
+                {64, 63},
+                {62, 61, 60},
+                {59, 58, 57, 56},
+                {55, 54, 53, 52, 51},
+                {50, 49, 48, 47, 46, 45},
+                {44, 43, 42, 41, 40, 39, 38},
+                {37, 36, 35, 34, 33, 32, 31, 30},
+                {29, 28, 27, 26, 25, 24, 23, 22, 21},
+                {20, 19, 18, 17, 16, 15, 14, 13, 12, 11}
+        };
+        int allAge = 0;
+        int count = 0;
+        int years = 0;
+        int midlife;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++){
+                allAge = allAge + array[i][j];
+                System.out.print(array[i][j] + " ");
+                count++;
+            }
+            System.out.println();
+            years++;
+        }
+        midlife = allAge / count;
+        System.out.println();
+        System.out.println("Общий возраст: " + allAge + " лет");
+        System.out.println("Численность отряда: " + count + " человек");
+        System.out.println("Средний возраст: " + midlife + " лет");
+        System.out.println("Отряд существует: " + years + " лет");
+        System.out.println();
+        double allAge2 = allAge - 11;
+        double count2 = count - 1;
+        double midlife2 = allAge2 / count2;
+        double changed = midlife2 - midlife;
+        System.out.println("Если 11-и летний воин не поступит на службу:");
+        System.out.println("Общий возраст: " + allAge2 + " лет");
+        System.out.println("Численность отряда: " + count2 + " человек");
+        System.out.println("Средний возраст: " + midlife2 + " лет");
+        System.out.println("Отряд существует: " + years + " лет");
+        System.out.println();
+        System.out.println("Как изменится средний возраст?");
+        System.out.println("Как изменится на: " + changed + " года");
+
     }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main2 {
+import java.util.Arrays;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int n = 11, year = 65, years = 0;
+        double allAge = 0, count = 0;
+        var matrix = new int[n][];
+        for (int i = 0; i < matrix.length; ++i) {
+            matrix[i] = new int[i];
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = year--;
+                allAge = allAge + matrix[i][j];
+                count++;
+            }
+            years++;
+        }
+        double midLife = allAge / count;
+        System.out.println(Arrays.deepToString(matrix));
+        System.out.println("Общее количество лет: " + allAge);
+        System.out.println("Численность отряда: " + count + " человек");
+        System.out.println("Средний воздаст в отряде: " + midLife + " лет");
+        System.out.println("Отряд существует: " + (years - 1) + " лет");
+        System.out.println();
+        System.out.println("Если последнего воина не возьмут в отряд, то: " +
+                "\nОтряд будет состоять из " + (count - 1) + " человек. " +
+                "\nСредний возраст отряда составит: " + ((allAge - 11) / (count - 1)) + " лет. " +
+                "\nИзменится средний возраст на " + (((allAge - 11) / (count - 1)) - midLife) + " года");
+    }
 }
